01186103c2ef43ac44b988e3a6085070d449c6f6
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-12-23 07:48:01 +0100

    simplelog

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
 mod models;
 
-fn main() {
-    env_logger::init();
+use models::config::Configuration;
+use simplelog::{SimpleLogger, Config, LevelFilter};
+use log::info;
+
+#[tokio::main]
+async fn main(){
     println!("Hello, world!");
+    let configuration = Configuration::read_configuration().await;
+    let level_filter = match configuration.get_log_level(){
+        "error" => LevelFilter::Error,
+        "warn" => LevelFilter::Warn,
+        "info" => LevelFilter::Info,
+        "debug" => LevelFilter::Debug,
+        "trace" => LevelFilter::Trace,
+        _ => LevelFilter::Off,
+    };
+    let _ = SimpleLogger::init(level_filter, Config::default());
+    info!("Configuration: {}", configuration);
 }
