9785eb4175cae8cbdfe01d670e2be54ba59bdeca
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-12-27 13:32:14 +0100

    change config

--- src/models/site.rs
+++ src/models/site.rs
@@ -3,36 +3,21 @@ use serde::{Serialize, Deserialize};
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Site{
     pub author: String,
-    pub name: String,
+    pub title: String,
     pub description: String,
     pub baseurl: String,
+    pub url: String,
     pub avatar: String,
-    pub google_analytics: String,
-    pub disqus: String,
     pub category: String,
     pub subcategory: String,
     pub explicit: bool,
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct FooterLinks{
-    pub dribble: String,
     pub email: String,
-    pub facebook: String,
-    pub flickr: String,
     pub gitlab: String,
-    pub instagram: String,
-    pub linkedin: String,
-    pub pinterest: String,
     pub rss: String,
     pub twitter: String,
-    pub stackoverflow: String,
-    pub youtube: String,
-    pub googleplus: String,
-    pub telegram: String,
     pub archiveorg: String,
-    pub mastodon: String,
-    pub android: String,
+    pub google_analytics: String,
+    pub disqus: String,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
