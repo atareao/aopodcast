0053f5d0b175d7d53ff70074b6ad479012c37b7f
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2023-01-07 08:07:53 +0100

    excerpt to words

--- src/models/utils.rs
+++ src/models/utils.rs
@@ -1,36 +1,15 @@
 use regex::Regex;
 use chrono::{DateTime, Utc, NaiveDate, NaiveTime, NaiveDateTime};
-use log::{info, debug};
+use log::debug;
 
-const EXCERPT_LENGTH: usize = 150;
+const EXCERPT_LENGTH: usize = 55;
 
-pub fn get_excerpt(content: &str) -> String{
-    debug!("Content ({}): {}", content.len(), content);
-    if content.len() > EXCERPT_LENGTH{
-        let excerpt = content
-            .split("\n")
-            .collect::<Vec<&str>>()
-            .get(0)
-            .unwrap()
-            .to_string();
-        debug!("Sort description: item");
-        if excerpt.len() > EXCERPT_LENGTH{
-           excerpt.as_str()
-                .chars()
-                .into_iter()
-                .take(EXCERPT_LENGTH)
-                .collect::<String>()
-                .to_string()
-        }else{
-            excerpt
-        }
-    }else{
-        content.to_string()
-    }
+pub fn get_excerpt<'a>(content: &'a str) -> &'a str{
+    get_first_words(content, EXCERPT_LENGTH)
 }
 
 pub fn get_slug(title: &str) -> String{
-    info!("Slug from: '{}'", title);
+    debug!("Slug from: '{}'", title);
     let title: String = title
         .to_lowercase().
         chars()
@@ -64,6 +43,26 @@ pub fn get_slug(title: &str) -> String{
     }
 }
 
+pub fn get_first_words<'a>(content: &'a str, number: usize) -> &'a str{
+    let re1 = Regex::new(r"[ ]{2,}").unwrap();
+    let re2 = Regex::new(r"[\n]{2,}").unwrap();
+    let re3 = Regex::new(r"[\t]{2,}").unwrap();
+    let clean_content = re1.replace_all(&content, " ").to_string();
+    let clean_content = re2.replace_all(&clean_content, "\n").to_string();
+    let clean_content = re3.replace_all(&clean_content, "\t").to_string();
+    let positions = clean_content.chars()
+        .enumerate()
+        .filter(|(_, c)| *c == ' ' || *c == '\n' || *c == '\t')
+        .map(|(i, _)| i)
+        .collect::<Vec<_>>();
+    if positions.len() > number{
+        let position = positions[number];
+        &content[..position].trim()
+    }else{
+        content.trim()
+    }
+}
+
 pub fn get_date(mtime: &str) -> String{
     let timestamp = mtime.parse::<i64>().unwrap();
     let naive_date_time = NaiveDateTime::from_timestamp_opt(timestamp, 0).unwrap();
@@ -87,4 +86,12 @@ fn test_get_unix_time(){
     let ut = get_unix_time(date);
     println!("ut: {}", ut);
     assert_ne!(ut, 1);
+}
+
+#[test]
+fn test_get_first_words(){
+    let content = "En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor. Una olla de algo más vaca que carnero, salpicón las más noches, duelos y quebrantos los sábados, lantejas los viernes, algún palomino de añadidura los domingos, consumían las tres partes de su hacienda. El resto della concluían sayo de velarte, calzas de velludo para las fiestas, con sus pantuflos de lo mesmo, y los días de entresemana se honraba con su vellorí de lo más fino. Tenía en su casa una ama que pasaba de los cuarenta, y una sobrina que no llegaba a los veinte, y un mozo de campo y plaza, que así ensillaba el rocín como tomaba la podadera. Frisaba la edad de nuestro hidalgo con los cincuenta años; era de complexión recia, seco de carnes, enjuto de rostro, gran madrugador y amigo de la caza. Quieren decir que tenía el sobrenombre de Quijada, o Quesada, que en esto hay alguna diferencia en los autores que deste caso escriben; aunque por conjeturas verosímiles se deja entender que se llamaba Quijana. Pero esto importa poco a nuestro cuento: basta que en la narración dél no se salga un punto de la verdad.";
+    let fw = get_first_words(content, 55);
+    println!("FW: {}", fw);
+    assert_ne!(fw.len(), 55);
 }
