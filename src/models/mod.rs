352618fb03070f52db3ca7aba4c47c31e9fa82ac
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2024-03-30 09:50:02 +0100

    clean

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -11,17 +11,7 @@ pub mod utils;
 pub mod mastodon;
 pub mod telegram;
 
-pub use archive::ArchiveOrg;
-pub use article::Article;
-pub use config::Configuration;
-pub use doc::Doc;
-pub use episode::{Episode, Metadata};
-pub use mastodon::{Mastodon, get_mastodon_client};
-pub use metadata::AOMetadata;
-pub use mp3metadata::Mp3Metadata;
-pub use page::Page;
-pub use site::{Layout, Site, Post};
-pub use telegram::{Telegram, get_telegram_client};
+pub use site::{Layout, Post};
 
 use minijinja::{Environment, path_loader};
 use once_cell::sync::Lazy;
